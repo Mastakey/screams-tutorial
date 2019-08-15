2396e62cb573a3782e1afba54946f4df878d9d9b
Han <Kioh.Han@espn.com>
2019-08-14 22:12:22 -0400

    Init

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2019-08-13T18:02:54.810Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
