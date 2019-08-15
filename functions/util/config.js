cffc17da2e4638f5cc3f29d70b33c49aabe5920f
Kioh Han <kioh.han@gmail.com>
2019-08-14 23:30:26 -0400

    separate to files

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyBw3bf149FNKR_sfGy6y264EEaABvIucBw",
+    authDomain: "social-ape-tutorial-960c4.firebaseapp.com",
+    databaseURL: "https://social-ape-tutorial-960c4.firebaseio.com",
+    projectId: "social-ape-tutorial-960c4",
+    storageBucket: "social-ape-tutorial-960c4.appspot.com",
+    messagingSenderId: "974351179986",
+    appId: "1:974351179986:web:f350ae1ed4f38b5b"
+};
