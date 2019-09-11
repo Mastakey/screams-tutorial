cbe8048a34d32985bdd39ab42b21ed8457cc5990
Kioh Han <kioh.han@gmail.com>
2019-09-10 23:18:45 -0400

    cors

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,9 @@ const app = express();
 
 const FBAuth = require('./util/fbAuth');
 
+const cors = require('cors');
+app.use(cors());
+
 const { db } = require('./util/admin');
 const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream } = require('./handlers/screams');
 const { signUp, login, uploadImage, addUserDetails, getAuthenticatedUser, getUserDetails, markNotificationsRead } = require('./handlers/users');
